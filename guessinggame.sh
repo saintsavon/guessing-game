b1d90a6d8b4b505a0c7cda1c5e4dcee75ede52af
jaylensavon <jaylensavon@gmail.com>
2020-02-05 09:25:20 -0800

    updated guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,10 +1,25 @@
-echo "Please guess how many files are in the current directory:"
-read $response
+function main(){
+while true [[ $count -ne $guess ]]
+do
+	read -p "Please guess how many files that are in the current directory: " guess
+	if [[ $guess -lt $count ]]
+	then
+		echo "Your guess was too low!"
+	elif [[ $guess -gt $count ]]
+	then
+		echo "Your guess was too high!"
+	elif [[ $guess -eq $count ]]
+	then
+		echo "Your guess was correct! Congratulations!"
+	break
+	else
+		echo "Please input a valid variable (Integer)."
+	fi
+done
+}
 
-if [[ $response -eq 3 ]]
-then
-	echo "You guessed correctly!"
-else
-	echo "Incorrect! Please guess again:"
-fi
+count=3
+guess=-1
+main
 
+echo "Thanks for playing!"
